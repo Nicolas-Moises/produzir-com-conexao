6038f457c3950de1e2cdc774d763ea70d98d88b1
Nicolas Moises <nicolas.nicc21@gmail.com>
2023-09-25 13:09:08 -0300

    fix: window is not defined

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -36,8 +36,9 @@ export function Header() {
             setColor(true)
         } else setColor(false)
     }
-
-    window.addEventListener('scroll', changeColor)
+    if (window !== undefined) {
+        window.addEventListener('scroll', changeColor)
+    }
     return (
         <header className={`w-full px-4 fixed top-0 left-0 right-0 z-50 ${color ? 'backdrop-blur-xl bg-zinc-900/70' : 'bg-zinc-900'} transition-colors duration-150`}>
             <div className="flex items-center justify-center lg:justify-between w-full max-w-7xl mx-auto">
